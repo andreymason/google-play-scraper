da6830715faf3f3d1409524b5eb463e336535be7
Mason <48023036+andreymason@users.noreply.github.com>
2025-10-27 13:50:37 +0200

    Refactor _urlopen to use requests with optional proxy

--- google_play_scraper/utils/request.py
+++ google_play_scraper/utils/request.py
@@ -1,5 +1,6 @@
 import ssl
 import time
+import requests
 from typing import Union
 from urllib.error import HTTPError
 from urllib.request import Request, urlopen
@@ -11,10 +12,34 @@ ssl._create_default_https_context = ssl._create_unverified_context
 MAX_RETRIES = 3
 RATE_LIMIT_DELAY = 5
 
-
-def _urlopen(obj):
+def _urlopen(url, proxy = None):
+    resp = ""
+    headers = {
+        "User-Agent": (
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/124.0.0.0 Safari/537.36"
+        )
+    }
     try:
-        resp = urlopen(obj)
+        if proxy:
+            response = requests.get(url, proxies=proxy, headers=headers)
+        else:
+            response = requests.get(url, headers=headers)
+        print(response.status_code)
+        if response.status_code == 200:
+            resp = response.text 
+        else:
+            print(f"Request failed with status code: {response.status_code}")
+
+    # except requests.exceptions.SSLError as e:
+    #     # Handling SSLError exception
+    #     print(f"SSL Error occurred: {e}")
+    #     return _urlopen(url, proxy)  
+    # except requests.exceptions.ProxyError as e:
+    #     # Handling ProxyError exception
+    #     print(f"Proxy Error occurred: {e}")
+    #     return _urlopen(url, proxy)  
     except HTTPError as e:
         if e.code == 404:
             raise NotFoundError("App not found(404).")
@@ -22,8 +47,7 @@ def _urlopen(obj):
             raise ExtraHTTPError(
                 "App not found. Status code {} returned.".format(e.code)
             )
-
-    return resp.read().decode("UTF-8")
+    return resp
 
 
 def post(url: str, data: Union[str, bytes], headers: dict) -> str:
@@ -44,5 +68,5 @@ def post(url: str, data: Union[str, bytes], headers: dict) -> str:
     raise last_exception
 
 
-def get(url: str) -> str:
-    return _urlopen(url)
+def get(url: str, proxy = None) -> str:
+    return _urlopen(url, proxy)
